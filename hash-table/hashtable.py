92f93d7746cd19d2e930f233e2d1757198514d87
Noka993 <noka993@wp.pl>
2025-02-18 17:46:37 +0100

    Upgraded hash table to use separate chaining

--- hash-table/hashtable.py
+++ hash-table/hashtable.py
@@ -1,26 +1,68 @@
 from typing import NamedTuple, Any
+from collections import deque
+
+DELETED = object()
 
 class Pair(NamedTuple):
     key: Any
     value: Any
     
 class HashTable:
-    def __init__(self, capacity):
+    def __init__(self, capacity=8, load_factor_threshold=0.6):
         if capacity < 1:
             raise ValueError("Capacity must be a positive number")
-        self._pairs = capacity * [None]
+        if not (0 < load_factor_threshold <= 1):
+            raise ValueError("Load factor must be a number between (0, 1]")
+        # For linear probing hash collision approach
+        # self._slots = capacity * [None]
+        self._keys = []
+        self._buckets = [deque() for _ in range(capacity)]
+        self._load_factor_threshold = load_factor_threshold
     
     def __len__(self):
         return len(self.pairs)
     
+    # def __setitem__(self, key, value):
+    #     if self.load_factor >= self._load_factor_threshold:
+    #         self._resize_and_rehash()
+
+    #     for index, pair in self._probe(key):
+    #         if pair is DELETED: continue
+    #         if pair is None or pair.key == key:
+    #             self._slots[index] = Pair(key, value)
+    #             break
+    
+    # Separate chaining
     def __setitem__(self, key, value):
-        self._pairs[self._index(key)] = Pair(key, value)
+        if self.load_factor >= self._load_factor_threshold:
+            self._resize_and_rehash()
+
+        bucket = self._buckets[self._index(key)]
+        for index, pair in enumerate(bucket):
+            if pair.key == key:
+                bucket[index] = Pair(key, value)
+                break
+        else:
+            bucket.append(Pair(key, value))
+            self._keys.append(key)
         
+    # def __getitem__(self, key):
+    #     for _, pair in self._probe(key):
+    #         if pair is None:
+    #             raise KeyError(key)
+    #         if pair is DELETED:
+    #             continue
+    #         if pair.key == key:
+    #             return pair.value
+    #     raise KeyError(key)
+    
+    # Separate chaining
     def __getitem__(self, key):
-        pair = self._pairs[self._index(key)]
-        if pair is None:
-            raise KeyError(key)
-        return pair.value
+        bucket = self._buckets[self._index(key)]
+        for pair in bucket:
+            if pair.key == key:
+                return pair.value
+        raise KeyError(key)
     
     def __contains__(self, key):
         try:
@@ -36,9 +78,26 @@ class HashTable:
         except KeyError:
             return default
         
+    # def __delitem__(self, key):
+    #     for index, pair in self._probe(key):
+    #         if pair is None:
+    #             raise KeyError(key)
+    #         if pair is DELETED:
+    #             continue
+    #         if pair.key == key:
+    #             self._slots[index] = DELETED
+    #             break
+    #     else:
+    #         raise KeyError(key)
+    
+    # Separate chaining
     def __delitem__(self, key):
-        if key in self:
-            self._pairs[self._index(key)] = None
+        bucket = self._buckets[self._index(key)]
+        for index, pair in enumerate(bucket):
+            if pair.key == key:
+                del bucket[index]
+                self._keys.remove(key)
+                break
         else:
             raise KeyError(key)
     
@@ -55,28 +114,76 @@ class HashTable:
         cls = self.__class__.__name__
         return f"{cls}.from_dict({str(self)})"
     
+    def __eq__(self, other):
+        if self is other:
+            return True
+        if type(self) is not type(other):
+            return False
+        return set(self.pairs) == set(other.pairs)
+
+    # @property
+    # def pairs(self):
+    #     return {
+    #         pair for pair in self._slots
+    #         if pair not in (None, DELETED)
+    #     }
+      
+    # Separate chaining  
     @property
     def pairs(self):
-        return {pair for pair in self._pairs if pair}
-    
+        return [(key, self[key]) for key in self.keys]
+
     @property
     def values(self):
-        return [pair.value for pair in self.pairs]
+        return [self[key] for key in self.keys]
     
     @property
     def keys(self):
-        return {pair.key for pair in self.pairs}
+        return self._keys.copy()
 
+    # @property
+    # def capacity(self):
+    #     return len(self._slots)
+    
+    # Separate chaining  
     @property
     def capacity(self):
-        return len(self._pairs)
+        return len(self._buckets)
     
     @classmethod
     def from_dict(cls, dictionary, capacity=None):
-        hash_table = cls(capacity or len(dictionary) * 10)
+        hash_table = cls(capacity or len(dictionary))
         for key, value in dictionary.items():
             hash_table[key] = value
         return hash_table
         
     def _index(self, key):
         return hash(key) % self.capacity
+    
+    def _probe(self, key):
+        index = self._index(key)
+        for _ in range(self.capacity):
+            yield index, self._slots[index]
+            index = (index + 1) % self.capacity
+
+    def _resize_and_rehash(self):
+        copy = HashTable(capacity=self.capacity * 2)
+        for key, value in self.pairs:
+            copy[key] = value
+        # For linear probing hash collision approach
+        # self._slots = copy._slots
+        self._buckets = copy._buckets
+
+    
+    def copy(self):
+        return HashTable.from_dict(dict(self.pairs), self.capacity)
+    
+    # @property
+    # def load_factor(self):
+    #     occupied_or_deleted = [slot for slot in self._slots if slot]
+    #     return len(occupied_or_deleted) / self.capacity
+    
+    # Separate chaining
+    @property
+    def load_factor(self):
+        return len(self) / self.capacity
--- hash-table/test_hashtable.py
+++ hash-table/test_hashtable.py
@@ -1,6 +1,7 @@
 from hashtable import HashTable
 import pytest
 from pytest_unordered import unordered
+from unittest.mock import patch
 
 @pytest.fixture
 def hash_table():
@@ -17,7 +18,7 @@ def test_should_report_capacity():
     assert HashTable(capacity=100).capacity == 100
     
 def test_should_create_empty_value_slots():
-    assert HashTable(capacity=3)._pairs == [None] * 3
+    assert HashTable(capacity=3)._slots == [None] * 3
     
 def test_should_insert_key_value_pairs():
     hash_table = HashTable(capacity=100)
@@ -203,3 +204,40 @@ def test_should_have_canonical_string_representation(hash_table):
         "HashTable.from_dict({False: True, 'hola': 'hello', 98.6: 37})",
         "HashTable.from_dict({False: True, 98.6: 37, 'hola': 'hello'})",
     }
+    
+def test_should_compare_equal_to_itself(hash_table):
+    assert hash_table == hash_table
+
+def test_should_compare_equal_to_copy(hash_table):
+    assert hash_table is not hash_table.copy()
+    assert hash_table == hash_table.copy()
+
+def test_should_compare_equal_different_key_value_order(hash_table):
+    h1 = HashTable.from_dict({"a": 1, "b": 2, "c": 3})
+    h2 = HashTable.from_dict({"b": 2, "a": 1, "c": 3})
+    assert h1 == h2
+
+def test_should_compare_unequal(hash_table):
+    other = HashTable.from_dict({"different": "value"})
+    assert hash_table != other
+
+def test_should_compare_unequal_another_data_type(hash_table):
+    assert hash_table != 42
+
+def test_should_copy_keys_values_pairs_capacity(hash_table):
+    copy = hash_table.copy()
+    assert copy is not hash_table
+    assert set(hash_table.keys) == set(copy.keys)
+    assert unordered(hash_table.values) == copy.values
+    assert set(hash_table.pairs) == set(copy.pairs)
+    assert hash_table.capacity == copy.capacity
+
+def test_should_compare_equal_different_capacity():
+    data = {"a": 1, "b": 2, "c": 3}
+    h1 = HashTable.from_dict(data, capacity=50)
+    h2 = HashTable.from_dict(data, capacity=100)
+    assert h1 == h2
+    
+@patch("builtins.hash", return_value=42)
+def test_should_detect_hash_collision(hash_mock):
+    assert hash("foobar") == 42
