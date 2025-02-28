787b3f238cc5da2fc363f5b906f7ae2fddd83a54
Noka993 <Noka993@wp.pl>
2025-02-28 19:38:41 +0100

    Added pandas data analysis exercise

--- regular-expressions/babynames.py
+++ regular-expressions/babynames.py
@@ -34,60 +34,62 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
+
 def extract_names(filename):
-  """
-  Given a file name for baby.html, returns a list starting with the year string
-  followed by the name-rank strings in alphabetical order.
-  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
-  """
-  
-  content = ''
-  with open(filename) as file:
-    content = str(file.readlines())
-  
-  year = re.findall(r'\sin\s\d\d\d\d', content)[0][4:]
-  file_data = re.findall(r'<td>\w*</td>', content)
-  names = []
-  seen_names = set()
-  current_number = 0
-  for i, word in enumerate(file_data):
-    word = word[4:-5]
+    """
+    Given a file name for baby.html, returns a list starting with the year string
+    followed by the name-rank strings in alphabetical order.
+    ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+    """
 
-    if i % 3 != 0 and word not in seen_names:
-      names.append(word + " " + current_number)
-      seen_names.add(word)
-    else:
-      current_number = word
+    content = ""
+    with open(filename) as file:
+        content = str(file.readlines())
 
-  names.sort()
-  with open("babynames_results_"+ year + ".txt", "w") as file:
-    file.write(year + '\n')
-    file.write("\n".join(names))
+    year = re.findall(r"\sin\s\d\d\d\d", content)[0][4:]
+    file_data = re.findall(r"<td>\w*</td>", content)
+    names = []
+    seen_names = set()
+    current_number = 0
+    for i, word in enumerate(file_data):
+        word = word[4:-5]
 
-  return
+        if i % 3 != 0 and word not in seen_names:
+            names.append(word + " " + current_number)
+            seen_names.add(word)
+        else:
+            current_number = word
+
+    names.sort()
+    with open("babynames_results_" + year + ".txt", "w") as file:
+        file.write(year + "\n")
+        file.write("\n".join(names))
+
+    return
 
 
 def main():
-  # This command-line parsing code is provided.
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
+    # This command-line parsing code is provided.
+    # Make a list of command line arguments, omitting the [0] element
+    # which is the script itself.
+    args = sys.argv[1:]
 
-  if not args:
-    print('usage: [--summaryfile] file [file ...]')
-    sys.exit(1)
+    if not args:
+        print("usage: [--summaryfile] file [file ...]")
+        sys.exit(1)
 
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
+    # Notice the summary flag and remove it from args if it is present.
+    summary = False
+    if args[0] == "--summaryfile":
+        summary = True  # noqa: F841
+        del args[0]
 
-  # +++your code here+++
-  # For each filename, get the names, then either print the text output
-  # or write it to a summary file
-  for year in range(1990, 2009, 2):
-    extract_names('baby_names_examples/baby' + str(year) + '.html')
+    # +++your code here+++
+    # For each filename, get the names, then either print the text output
+    # or write it to a summary file
+    for year in range(1990, 2009, 2):
+        extract_names("baby_names_examples/baby" + str(year) + ".html")
 
-if __name__ == '__main__':
-  main()
+
+if __name__ == "__main__":
+    main()
